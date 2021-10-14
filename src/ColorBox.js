aed3f435a2bdf8ea52910a186f4d6198aaa9ee89
AaronJHarvey <Harvey.AaronJ@gmail.com>
2021-10-13 20:29:59 -0400

    fin

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 
+// export default class ColorBox extends Component {
+//   state = {
+//     todos: [
+//     ]
+//   }
+//   render() {
+//     if(this.props.opacity >= .2){
+//       return (
+//         <div className="color-box" style={{opacity: this.props.opacity}}>
+//           <ColorBox opacity={this.props.opacity -.1}/>
+//       </div>
+//     )
+//   } else {
+//     return null
+//     }
+//   }
+// }
+
+
 export default class ColorBox extends Component {
-
-  state = {
-    todos: [
-
-    ]
-  }
-
-  render() {
-    if(this.props.opacity >= .2){
-      return (
-        <div className="color-box" style={{opacity: this.props.opacity}}>
-          <ColorBox opacity={this.props.opacity -.1}/>
-      </div>
-
+  render(){
+    return(
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {this.props.opacity >= .2 ? <ColorBox opacity={this.props.opacity - .1}/> : null}
+        </div>
     )
-  } else {
-    return null
-    }
   }
-
 }
